3dfd6362751358f20b178375492247ed58026ed0
Sasja <Alexander.Steen.Gent@gmail.com>
2016-04-26 17:08:22 +0200

    check for socat in path

--- tcp/startSpaceService.sh
+++ tcp/startSpaceService.sh
@@ -8,18 +8,20 @@
 echo "this hasnt been tested yet!"
 exit 1
 
+which rocat > /dev/null || { echo "Must have socat installed!"; exit 1; }
+
+# if [[ $EUID -ne 0 ]]; then
+#     echo "You must be a root user"
+#     exit 1
+# fi
+
 PORT=1337
 #DEVICENAME=/dev/footty
 DEVICENAME=OPEN:tmp.txt,ignoreeof,append # just for testing
 WELCOMEMSG=welcome.txt
 INACT_TIMEOUT=10
 
-# if [[ $EUID -ne 0 ]]; then
-#     echo "You must be a root user" 2>&1
-#     exit 1
-# fi
-
 while true; do
     echo "starting new serial connection"
-    socat -T ${INACT_TIMEOUT} TCP4-LISTEN:${PORT},reuseaddr OPEN:${WELCOMEMSG},ignoreeof\!\!OPEN:${DEVICENAME},raw,echo=0 || echo retry in 10s; sleep 10
+    socat -T ${INACT_TIMEOUT} TCP4-LISTEN:${PORT},reuseaddr OPEN:${WELCOMEMSG},ignoreeof\!\!OPEN:${DEVICENAME},raw,echo=0 || { echo retry in 10s; sleep 10; }
 done
